3488e6a3e1fb3ddcc44ed66eac01c91cd85d1ad7
Svetlozar Mihailov <svetliomihailov@gmail.com>
2018-03-08 10:40:24 +0200

    Tweek the gfind function

--- shell/mac/helpers.sh
+++ shell/mac/helpers.sh
@@ -1,11 +1,17 @@
 gfind() {
   if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied"
-  else
-    path=`bundle show $1`
-    echo $path
-    `/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl $path`
+    then
+      echo "No arguments supplied"
+    else
+      path=`bundle show ${1}`
+
+      if [ $? -eq 0 ]
+        then
+          echo $path
+          `/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl $path`
+        else
+          echo "Gem not found"
+      fi
   fi
 }
 
