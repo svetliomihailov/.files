271afa049359ba336907a60419ed7d787599c866
Svetlozar Mihailov <svetliomihailov@gmail.com>
2020-05-10 18:41:43 +0300

    Update prompt

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -2,7 +2,7 @@ autoload -Uz vcs_info
 precmd_vcs_info() { vcs_info }
 precmd_functions+=( precmd_vcs_info )
 setopt prompt_subst
-zstyle ':vcs_info:git:*' formats '%F{#af5f00}%r: %b%f'
+zstyle ':vcs_info:git:*' formats '%F{#af5f00}%b%f'
 zstyle ':vcs_info:*' enable git
 
 PROMPT='%B%F{#808000}%~%f%b ${vcs_info_msg_0_} Σ '
